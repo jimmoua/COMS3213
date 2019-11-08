92e926029b35956328b0428c64afa3ae26e3bf36
Jim Moua <jmoua2@boole.cs.atu.edu>
2019-11-08 16:14:13 -0600

    Renamed AVL.cpp to a.cpp since that's what is required for assignment. Finished commenting. Final commit for this assignment.

--- assignments/05/a.cpp
+++ assignments/05/a.cpp
@@ -1,12 +1,20 @@
 ////////////////////////////////////////////////////////////////////////////////
 // Name: Jim Moua
+//
 // Program 05
+//
 // Description:
-// Special Processing Requirements: Requires 
+//   This program is a play with binary search trees - but not just any binary
+//   search tree. It is an AVL tree, a type of binary search tree in which the
+//   height of the left or right subtrees differ by at most one.
+//
+// Special Processing Requirements:
+//   Program requires C++11 to compile, since nullptrs are being used instead
+//   of the normal NULL for pointers.
 ////////////////////////////////////////////////////////////////////////////////
 
 //////////////////////////////
-// Libraries to include
+// Standard libraries include
 //////////////////////////////
 #include <iostream>
 #include <limits>
@@ -45,10 +53,13 @@ struct avl_node {
  
 ////////////////////////////////////////////////////////////////////////////////
 // Class: avlTree
+//
 // Description:
+// The avlTree class is the class in which instantiated objects will represent
+// the AVL tree.
 ////////////////////////////////////////////////////////////////////////////////
 class avlTree {
-  public:
+  private:
     int height(avl_node*);
     int diff(avl_node*);
     avl_node* rr_rotation(avl_node*);
@@ -56,11 +67,13 @@ class avlTree {
     avl_node* lr_rotation(avl_node*);
     avl_node* rl_rotation(avl_node*);
     avl_node* balance(avl_node*);
-    avl_node* insert(avl_node*, int);
+  public:
     void display(avl_node*, int);
     void inorder(avl_node*);
     void preorder(avl_node*);
     void postorder(avl_node*);
+    avl_node* insert(avl_node*, int);
+    // Default ctor
     avlTree() {
       root = nullptr;
     }
@@ -162,16 +175,12 @@ int main() {
   return 0;
 }
  
-/*
- * Height of AVL Tree
- */
 ////////////////////////////////////////////////////////////////////////////////
 // Function:
 // avlTree::height(avl_node* tmp)
 //
 // Description:
-// Computes the height of the tree via recursion. This means every subtree's
-// height will be calculated.
+// Computes the height of a subtree via recursion.
 // 
 // Preconditions:
 // None
@@ -194,11 +203,15 @@ int avlTree::height(avl_node* temp) {
 // Function:
 // avlTree::diff(avl_node* tmp)
 //
+// Description:
+// Returns the difference in height of the left subtree and the right subtree.
+//
 // Preconditions:
 // None
 //
 // Postconditions:
-// None
+// Returns an integer - one that is the difference in heights of left and right
+// subtrees.
 ////////////////////////////////////////////////////////////////////////////////
 int avlTree::diff(avl_node* temp) {
   int l_height = height(temp->left);
@@ -206,9 +219,6 @@ int avlTree::diff(avl_node* temp) {
   return (l_height-r_height);
 }
  
-/*
- * Right- Right Rotation
- */
 ////////////////////////////////////////////////////////////////////////////////
 // Function:
 // avlTree::rr_rotation(avl_node* parent)
@@ -217,68 +227,27 @@ int avlTree::diff(avl_node* temp) {
 // Performs a right rotation about the parent node. The parent node is never
 // modified. The function returns a pointer called temp.
 //
-// temp points to the parent's right child, and temp's left child points to the
-// parent.
+// Preconditions:
+// None.
 //
-//       [P] [T] → [R]
-//       / \ /
-//     [L] [P]
-//
+// Postconditions:
+// Returns a node called temp as a parent node after right-right rotation has
+// been performed.
 ////////////////////////////////////////////////////////////////////////////////
-//
-//     [T] → [R]
-//     /
-//    [P]
-//
-////////////////////////////////////////////////////////////////////////////////
-//
-//     [T] → [R]
-//     /
-//    [P]
-//
-////////////////////////////////////////////////////////////////////////////////
-//
-//     [R] ← essentially, we are returning something like this (parent's right)
-//     /
-//    [P]
-//
-////////////////////////////////////////////////////////////////////////////////
-avl_node* avlTree::rr_rotation(avl_node *parent) {
-  // Create a pointer temp
-  //       [P]
-  //         \
-  //         [R]  [T]
-  //           \
-  //           [q]
+avl_node* avlTree::rr_rotation(avl_node* parent) {
+  // Create an avl_node claled temp
   avl_node* temp;
 
-  // Assign temp to the parent's right child
-  //       [P]
-  //         \
-  //         [R] ← [T]
-  //           \
-  //          [q]
+  // Temp points to parent's right child
   temp = parent->right;
 
-  // Assign parent's right child to temp's left child
-  //       [P]  [T] → [R]
-  //       / \  /
-  //     [L] [TL]
+  // Parent's right child points to temp's left child.
   parent->right = temp->left;
 
-  // Assign temp left to parent
-  //       [P] [T] → [R]
-  //       / \ /
-  //     [L] [P]
-  //
-  //       [P] [R]
-  //       / \ /
-  //     [L] [P]
+  // Temp's left child points to parent
   temp->left = parent;
 
-  // Returns temp
-  // temp is current pointing to the parent's right child
-  // temp's left child is pointing to parent node
+  // Returns the node temp, which acts as the node to a right-rotated subtree.
   return temp;
 }
 
@@ -288,92 +257,155 @@ avl_node* avlTree::rr_rotation(avl_node *parent) {
 //
 // Description:
 // Performs a left-left rotation.
+//
+// Preconditions:
+// None.
+//
+// Postconditions:
+// Returns a node called temp as a parent node after left-left rotation has
+// been applied.
 ////////////////////////////////////////////////////////////////////////////////
 avl_node *avlTree::ll_rotation(avl_node* parent) {
 
-  // Create an avl_node pointer called temp
-  //       [P]
-  //       / \
-  // [T] [L] [R]
-  //     / \
-  //   [l] [r]
+  // Create an avl_node called temp
   avl_node *temp;
 
-  // temp points to the parent's left child
-  //       [P]
-  //       / \
-  // [T]→[L] [R]
-  //     / \
-  //   [l] [r]
+  // temp points to parent's left child
   temp = parent->left;
 
-  // parent's left child points to temp's right child
-  //       [P]
-  //       / \
-  // [T]→[L] [R]
-  //     / \
-  //   [l] [r]
-  //
-  //       [P]
-  //       / \
-  //     [r] [R]    temp is pointing to left, so temp->right = Left->right
-  //     /
-  //   [l]
+  // parent's left child now points to temp's right child
   parent->left = temp->right;
 
-  //       [P]
-  //       / \
-  //     [r] [R]    temp is pointing to left, so temp->right = Left->right
-  //     /
-  //   [l]
+  // temp's right child points to parent
   temp->right = parent;
+
+  // return the temp node, which is essentially the left rotation about the
+  // parent.
   return temp;
+
 }
  
-/*
- * Left - Right Rotation
- */
-avl_node *avlTree::lr_rotation(avl_node *parent) {
-  avl_node *temp;
+////////////////////////////////////////////////////////////////////////////////
+// Function:
+// avlTree::lr_rotation(avl_node* parent)
+//
+// Description:
+// Performs a left-right rotation on a subtree about the parent node.
+//
+// Preconditions:
+// None.
+//
+// Postconditions:
+// Returns temp node after performing left-right rotation about the parent node.
+////////////////////////////////////////////////////////////////////////////////
+avl_node *avlTree::lr_rotation(avl_node* parent) {
+
+  // Create an avl_node called temp
+  avl_node* temp;
+
+  // temp points to the parent's left child.
   temp = parent->left;
-  parent->left = rr_rotation (temp);
-  return ll_rotation (parent);
+
+  // perform a right-right rotation on the temp node, and let parent's left
+  // child point to it
+  parent->left = rr_rotation(temp);
+
+  // Perform a left-left rotation on the parent node, and then return it.
+  return ll_rotation(parent);
+
 }
  
-/*
- * Right- Left Rotation
- */
-avl_node *avlTree::rl_rotation(avl_node *parent) {
-  avl_node *temp;
+////////////////////////////////////////////////////////////////////////////////
+// Function:
+// avlTree::rl_rotation(avl_node* parent)
+//
+// Description:
+// Performs a right-left rotation on a subtree about the parent node.
+//
+// Postconditions:
+//
+// Preconditions:
+// Returns a the parent node, which has been rotated via right-left rotation.
+////////////////////////////////////////////////////////////////////////////////
+avl_node* avlTree::rl_rotation(avl_node* parent) {
+
+  // Create an avl_node called temp
+  avl_node* temp;
+
+  // temp points to the parent's right child
   temp = parent->right;
-  parent->right = ll_rotation (temp);
-  return rr_rotation (parent);
+
+  // Perform a left-left rotation on the node temp, and then set parent's right
+  // child to point to it.
+  parent->right = ll_rotation(temp);
+
+  // Perform a right-right rotation on the node parent, and return it.
+  return rr_rotation(parent);
+
 }
  
-/*
- * Balancing AVL Tree
- */
-avl_node *avlTree::balance(avl_node *temp) {
+////////////////////////////////////////////////////////////////////////////////
+// Function:
+// avlTree::balance(avl_node* temp)
+//
+// Description:
+// Balances a subtree.
+//
+// Postconditions:
+// None.
+//
+// Preconditions:
+// Returns a node as the parent node after balancing the (sub)tree.
+////////////////////////////////////////////////////////////////////////////////
+avl_node* avlTree::balance(avl_node* temp) {
+
+  // Get the balance factor tree/subtree
   int bal_factor = diff (temp);
+
+  // If the balance factor is greater than one, we need to balance the tree
   if (bal_factor > 1) {
-    if (diff (temp->left) > 0)
-      temp = ll_rotation (temp);
-    else
-      temp = lr_rotation (temp);
+    // If the difference in height on the left is greater than 0, perform
+    // left-left rotation. Otherwise, perform a left-right rotation.
+    if (diff(temp->left) > 0) {
+      temp = ll_rotation(temp);
+    }
+    else {
+      temp = lr_rotation(temp);
+    }
   }
+  // The same applies here, that is, is the balance factor is less than -1, we
+  // need to balance the tree
   else if (bal_factor < -1) {
-    if (diff (temp->right) > 0)
+    if (diff(temp->right) > 0) {
       temp = rl_rotation (temp);
-    else
+    }
+    else {
       temp = rr_rotation (temp);
+    }
   }
+  
+  // Return node after performing rotations to balance it.
   return temp;
+
 }
  
-/*
- * Insert Element into the tree
- */
-avl_node *avlTree::insert(avl_node *root, int value) {
+////////////////////////////////////////////////////////////////////////////////
+// Function:
+// avlTree::insert(avlnode* root, int value)
+//
+// Description:
+// Inserts an avl_node with the provided value
+//
+// Preconditions:
+// None.
+//
+// Postconditions:
+// Returns root node after performing an insert.
+////////////////////////////////////////////////////////////////////////////////
+avl_node *avlTree::insert(avl_node* root, int value) {
+  // Keep traversing the tree until we've hit a nullptr. Once we've gotten to a
+  // node that is nullptr, we will allocate space for it, and assign a value to
+  // the node.
   if (root == nullptr) {
     root = new avl_node;
     root->data = value;
@@ -381,6 +413,8 @@ avl_node *avlTree::insert(avl_node *root, int value) {
     root->right = nullptr;
     return root;
   }
+  // Otherwise, perform recursive binary search to determine where to insert
+  // the node according to its value.
   else if (value < root->data) {
     root->left = insert(root->left, value);
     root = balance (root);
@@ -392,19 +426,29 @@ avl_node *avlTree::insert(avl_node *root, int value) {
   return root;
 }
  
-/*
- * Display AVL Tree
- */
-void avlTree::display(avl_node *ptr, int level)
+////////////////////////////////////////////////////////////////////////////////
+// Function:
+// avlTree:display(avlnode* ptr, int level)
+//
+// Description:
+// Prints the AVL tree.
+//
+// Preconditions:
+// None.
+//
+// Postconditions:
+// Traverserves the tree and displays in a tree-like format (but sideways).
+////////////////////////////////////////////////////////////////////////////////
+void avlTree::display(avl_node* ptr, int level)
 {
-  int i;
+  // If the tree isn't empty, print the tree
   if (ptr!=nullptr) {
     display(ptr->right, level+1);
     printf("\n");
     if (ptr == root) {
       std::cout<<"Root -> ";
     }
-    for (i = 0; i < level && ptr != root; i++) {
+    for (int i = 0; i < level && ptr != root; i++) {
       std::cout<<"        ";
     }
     std::cout << ptr->data;
@@ -412,23 +456,45 @@ void avlTree::display(avl_node *ptr, int level)
   }
 }
  
-/*
- * Inorder Traversal of AVL Tree
- */
-void avlTree::inorder(avl_node *tree)
+////////////////////////////////////////////////////////////////////////////////
+// Function:
+// avlTree:inorder(avl_node* tree)
+//
+// Description:
+// Perform an inorder traversal on the tree, and print the values in the nodes
+// while doing inorder traversal.
+//
+// Preconditions:
+// None.
+//
+// Postconditions:
+// Performs inorder traversal on a tree.
+////////////////////////////////////////////////////////////////////////////////
+void avlTree::inorder(avl_node* tree)
 {
   if(tree == nullptr) {
     return;
   }
-  inorder (tree->left);
-  std::cout<<tree->data<<"  ";
-  inorder (tree->right);
+  inorder(tree->left);
+  std::cout << tree->data << "  ";
+  inorder(tree->right);
 }
 
-/*
- * Preorder Traversal of AVL Tree
- */
-void avlTree::preorder(avl_node *tree) {
+////////////////////////////////////////////////////////////////////////////////
+// Function:
+// avlTree:preorder(avl_node* tree)
+//
+// Description:
+// Perform an preorder traversal on the tree, and print the values in the nodes
+// while doing preorder traversal.
+//
+// Preconditions:
+// None.
+//
+// Postconditions:
+// Perfoms preorder traversal on a tree.
+////////////////////////////////////////////////////////////////////////////////
+void avlTree::preorder(avl_node* tree) {
   if (tree == nullptr) {
     return;
   }
@@ -437,10 +503,21 @@ void avlTree::preorder(avl_node *tree) {
   preorder(tree->right);
 }
  
-/*
- * Postorder Traversal of AVL Tree
- */
-void avlTree::postorder(avl_node *tree) {
+////////////////////////////////////////////////////////////////////////////////
+// Function:
+// avlTree:postorder(avl_node* tree)
+//
+// Description:
+// Perform an postorder traversal on the tree, and print the values in the
+// nodes while doing postorder traversal.
+//
+// Preconditions:
+// None.
+//
+// Postconditions:
+// Performs postorder traversal on a tree.
+////////////////////////////////////////////////////////////////////////////////
+void avlTree::postorder(avl_node* tree) {
   if (tree == nullptr) {
     return;
   }
